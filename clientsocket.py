9ff7480bae85496925290b16e02113704736003e
Silambaraselvan R <saransilambu007@gmail.com>
2024-09-21 21:09:07 +0530

    Delete clientsocket.py

--- clientsocket.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-c=socket.socket()
-c.connect(("localhost",9998))
-while True:
-    userinput = input("enter the input : ")
-    c.send(bytes(userinput,"utf-8"))
-
-print(c.recv(1024).decode())
